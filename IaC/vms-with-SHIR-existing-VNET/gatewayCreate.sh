d3f8379f14e09b1484077fb49bd216d60eb62c80
luisrac <luisrac@outlook.com>
2024-11-15 12:53:28 +0100

    Fix script error

--- IaC/vms-with-SHIR-existing-VNET/gatewayCreate.sh
+++ IaC/vms-with-SHIR-existing-VNET/gatewayCreate.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-az account show
+token=$(az account get-access-token --resource https://purview.azure.net --query accessToken -o tsv)
+ir=$(az rest --method put \
+       --url $scanEndpoint/integrationRuntimes/$integrationRuntimeName?api-version=2022-02-01-preview \
+       --body '{"name":"$integrationRuntimeName","kind":"SelfHosted","properties":{}}' \
+       --headers Authorization="Bearer $token" Content-Type="application/json")
+keys=$(az rest --method post \
+       --url $scanEndpoint/integrationRuntimes/$integrationRuntimeName/:listAuthKeys?api-version=2022-02-01-preview \
+       --headers Authorization="Bearer $token" Content-Type="application/json")
+echo $keys > $AZ_SCRIPTS_OUTPUT_PATH
